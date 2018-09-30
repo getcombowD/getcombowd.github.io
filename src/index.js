6a13e9978011c1a0fa975b71976a15a1a5c8019a
Luke McKone <Lukemckone@gmail.com>
2018-09-30 15:27:40 +0100

    first website

---
+++ src/index.js
@@ -0,0 +1,8 @@
+document.getElementById("app").innerHTML = `
+<h1>Hello A Test Website Here</h1>
+<div>
+  Im You Like Photos Please 
+  <a href="harveystreetphotography.cf" target="_blank" rel="noopener noreferrer">CLICK HERE</a>
+  .To Get Fowarded To HaverStreetPhotography.cf Thank You
+</div>
+`;
