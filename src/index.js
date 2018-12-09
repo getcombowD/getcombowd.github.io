236620167bd04311010171ec588a1e8d8fde6067
Luke McKone <Lukemckone@gmail.com>
2018-12-09 14:26:27 +0000

    LOL
    XD

--- src/index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("app").innerHTML = `
-<h1>Hello A Test Website Here</h1>
-<div>
-  Im You Like Photos Please 
-  <a href="harveystreetphotography.cf" target="_blank" rel="noopener noreferrer">CLICK HERE</a>
-  .To Get Fowarded To HaverStreetPhotography.cf Thank You
-</div>
-`;
