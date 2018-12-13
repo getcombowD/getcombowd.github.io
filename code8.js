f7751b5a54e0a41c0668868b2fb394e1ab691ea2
Luke McKone <Lukemckone@gmail.com>
2018-12-13 20:22:58 +0000

    Missing in space
    dswasd

---
+++ code8.js
@@ -0,0 +1,105 @@
+gdjs.ControlsCode = {};
+gdjs.ControlsCode.GDJUMP2Objects1= [];
+gdjs.ControlsCode.GDJUMP2Objects2= [];
+gdjs.ControlsCode.GDJumpObjects1= [];
+gdjs.ControlsCode.GDJumpObjects2= [];
+gdjs.ControlsCode.GDRightObjects1= [];
+gdjs.ControlsCode.GDRightObjects2= [];
+gdjs.ControlsCode.GDLEFTObjects1= [];
+gdjs.ControlsCode.GDLEFTObjects2= [];
+gdjs.ControlsCode.GDNewObjectObjects1= [];
+gdjs.ControlsCode.GDNewObjectObjects2= [];
+gdjs.ControlsCode.GDNewObject2Objects1= [];
+gdjs.ControlsCode.GDNewObject2Objects2= [];
+gdjs.ControlsCode.GDComputerObjects1= [];
+gdjs.ControlsCode.GDComputerObjects2= [];
+gdjs.ControlsCode.GDBTNleftObjects1= [];
+gdjs.ControlsCode.GDBTNleftObjects2= [];
+gdjs.ControlsCode.GDBtnJumpObjects1= [];
+gdjs.ControlsCode.GDBtnJumpObjects2= [];
+gdjs.ControlsCode.GDBtnRightObjects1= [];
+gdjs.ControlsCode.GDBtnRightObjects2= [];
+gdjs.ControlsCode.GDTABLETObjects1= [];
+gdjs.ControlsCode.GDTABLETObjects2= [];
+gdjs.ControlsCode.GDPhoneObjects1= [];
+gdjs.ControlsCode.GDPhoneObjects2= [];
+gdjs.ControlsCode.GDCopyOfNewObject32Objects1= [];
+gdjs.ControlsCode.GDCopyOfNewObject32Objects2= [];
+gdjs.ControlsCode.GDNewObject3Objects1= [];
+gdjs.ControlsCode.GDNewObject3Objects2= [];
+gdjs.ControlsCode.GDNewObject4Objects1= [];
+gdjs.ControlsCode.GDNewObject4Objects2= [];
+gdjs.ControlsCode.GDCopyOfNewObject5Objects1= [];
+gdjs.ControlsCode.GDCopyOfNewObject5Objects2= [];
+gdjs.ControlsCode.GDNewObject5Objects1= [];
+gdjs.ControlsCode.GDNewObject5Objects2= [];
+gdjs.ControlsCode.GDNewObject6Objects1= [];
+gdjs.ControlsCode.GDNewObject6Objects2= [];
+
+gdjs.ControlsCode.conditionTrue_0 = {val:false};
+gdjs.ControlsCode.condition0IsTrue_0 = {val:false};
+gdjs.ControlsCode.condition1IsTrue_0 = {val:false};
+
+
+gdjs.ControlsCode.mapOfGDgdjs_46ControlsCode_46GDNewObject6Objects1Objects = Hashtable.newFrom({"NewObject6": gdjs.ControlsCode.GDNewObject6Objects1});gdjs.ControlsCode.eventsList0xb0aa8 = function(runtimeScene) {
+
+{
+
+gdjs.ControlsCode.GDNewObject6Objects1.createFrom(runtimeScene.getObjects("NewObject6"));
+
+gdjs.ControlsCode.condition0IsTrue_0.val = false;
+{
+gdjs.ControlsCode.condition0IsTrue_0.val = gdjs.evtTools.input.cursorOnObject(gdjs.ControlsCode.mapOfGDgdjs_46ControlsCode_46GDNewObject6Objects1Objects, runtimeScene, true, false);
+}if (gdjs.ControlsCode.condition0IsTrue_0.val) {
+{gdjs.evtTools.runtimeScene.replaceScene(runtimeScene, "Menu", false);
+}}
+
+}
+
+
+}; //End of gdjs.ControlsCode.eventsList0xb0aa8
+
+
+gdjs.ControlsCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+gdjs.ControlsCode.GDJUMP2Objects1.length = 0;
+gdjs.ControlsCode.GDJUMP2Objects2.length = 0;
+gdjs.ControlsCode.GDJumpObjects1.length = 0;
+gdjs.ControlsCode.GDJumpObjects2.length = 0;
+gdjs.ControlsCode.GDRightObjects1.length = 0;
+gdjs.ControlsCode.GDRightObjects2.length = 0;
+gdjs.ControlsCode.GDLEFTObjects1.length = 0;
+gdjs.ControlsCode.GDLEFTObjects2.length = 0;
+gdjs.ControlsCode.GDNewObjectObjects1.length = 0;
+gdjs.ControlsCode.GDNewObjectObjects2.length = 0;
+gdjs.ControlsCode.GDNewObject2Objects1.length = 0;
+gdjs.ControlsCode.GDNewObject2Objects2.length = 0;
+gdjs.ControlsCode.GDComputerObjects1.length = 0;
+gdjs.ControlsCode.GDComputerObjects2.length = 0;
+gdjs.ControlsCode.GDBTNleftObjects1.length = 0;
+gdjs.ControlsCode.GDBTNleftObjects2.length = 0;
+gdjs.ControlsCode.GDBtnJumpObjects1.length = 0;
+gdjs.ControlsCode.GDBtnJumpObjects2.length = 0;
+gdjs.ControlsCode.GDBtnRightObjects1.length = 0;
+gdjs.ControlsCode.GDBtnRightObjects2.length = 0;
+gdjs.ControlsCode.GDTABLETObjects1.length = 0;
+gdjs.ControlsCode.GDTABLETObjects2.length = 0;
+gdjs.ControlsCode.GDPhoneObjects1.length = 0;
+gdjs.ControlsCode.GDPhoneObjects2.length = 0;
+gdjs.ControlsCode.GDCopyOfNewObject32Objects1.length = 0;
+gdjs.ControlsCode.GDCopyOfNewObject32Objects2.length = 0;
+gdjs.ControlsCode.GDNewObject3Objects1.length = 0;
+gdjs.ControlsCode.GDNewObject3Objects2.length = 0;
+gdjs.ControlsCode.GDNewObject4Objects1.length = 0;
+gdjs.ControlsCode.GDNewObject4Objects2.length = 0;
+gdjs.ControlsCode.GDCopyOfNewObject5Objects1.length = 0;
+gdjs.ControlsCode.GDCopyOfNewObject5Objects2.length = 0;
+gdjs.ControlsCode.GDNewObject5Objects1.length = 0;
+gdjs.ControlsCode.GDNewObject5Objects2.length = 0;
+gdjs.ControlsCode.GDNewObject6Objects1.length = 0;
+gdjs.ControlsCode.GDNewObject6Objects2.length = 0;
+
+gdjs.ControlsCode.eventsList0xb0aa8(runtimeScene);
+return;
+}
+gdjs['ControlsCode'] = gdjs.ControlsCode;
